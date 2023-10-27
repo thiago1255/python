8d10fa9994f5dd6f6c9b3048352fa6e29d248842
thiago1255 <30846971+thiago1255@users.noreply.github.com>
2023-10-27 11:32:28 -0300

    more string examples

--- strings.py
+++ strings.py
@@ -1,4 +1,20 @@
 string = " a b c f g "
-print(string)
-string.strip()
-print(string)
+print(1, string)
+string = string.strip()
+print(2, string)
+if "a" in string:
+	print(3, "a")
+if string.isdigit():
+	integer = int(string)
+	print(4, integer)
+string = string.replace(" ", "")
+print(5, string)
+string += "hijk"
+print(6, string)
+string += "\nlmnop"
+print(7, string)
+string += "\nqrst"
+print(8, string)
+string = string.split("\n", 1)
+print(9, type(string), string)
+print(10, string[1])
